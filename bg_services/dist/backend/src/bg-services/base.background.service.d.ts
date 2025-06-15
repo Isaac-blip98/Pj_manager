be28492db432b65109686366dced32a8e4a78a77
Markide1 <markndwiga@gmail.com>
2025-06-15 23:41:38 +0300

    Delete bg_services/dist directory

--- bg_services/dist/backend/src/bg-services/base.background.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from '@nestjs/common';
-export declare abstract class BaseBackgroundService {
-    protected readonly logger: Logger;
-    constructor(serviceName: string);
-    abstract start(): Promise<void>;
-    abstract stop(): Promise<void>;
-}
